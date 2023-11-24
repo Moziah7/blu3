2188cf6f0b76a0a4bf7ab68f9247a5c6031a0e8a
Moziah7 <Mosiah07007@gmail.com>
2023-11-24 14:10:13 +0300

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -2,11 +2,11 @@ export { useWidth } from "./useWidth";
 export { useHeight } from "./useHeight";
 export { useCurrentTime } from "./useCurrentTime";
 
+export { useSigner } from "./useSigner";
+export { getAddress } from "./getAddress";
 export { writeContract } from "./writeContract";
 export { readContract } from "./readContract";
 export { nativeBalance } from "./nativeBalance";
-export { getAddress } from "./getAddress";
-export { useSigner } from "./useSigner";
 
 export { explorerList } from "./explorerList";
 export { rpcList } from "./rpcList";
